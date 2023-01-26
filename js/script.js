eb4c0845aaec94035386fe7e5847960186ee4964
motorolla29 <eutyou@gmail.com>
2023-01-26 15:35:04 +0300

    changed XMLHttpRequest to fetch with formData

--- js/script.js
+++ js/script.js
@@ -242,31 +242,31 @@ function getZero(num) {
             
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
-
             const formData = new FormData(form);
 
-            const object = {};
+            /* const object = {};
 
             formData.forEach((value, key) => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
+            const json = JSON.stringify(object); */
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                /* headers: {
+                    'Content-type': 'application/json'
+                }, */
+                body: formData
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success); 
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
